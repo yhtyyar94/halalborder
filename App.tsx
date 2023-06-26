0f08eee37016e3f51f4519bca3d12a7eaf519394
Yhtyyar Annayev <70913322+yhtyyar94@users.noreply.github.com>
2023-06-26 17:21:25 +0200

    removed back button functions

--- App.tsx
+++ App.tsx
@@ -1,29 +1,6 @@
-import {useEffect} from 'react';
-import {Alert, BackHandler} from 'react-native';
 import WebView from 'react-native-webview';
 
 const App = () => {
-  useEffect(() => {
-    const backAction = () => {
-      Alert.alert('Nereye!', 'Çıkmak istediğinizden eminmisiniz?', [
-        {
-          text: 'Iptal',
-          onPress: () => null,
-          style: 'cancel',
-        },
-        {text: 'Evet', onPress: () => BackHandler.exitApp()},
-      ]);
-      return true;
-    };
-
-    const backHandler = BackHandler.addEventListener(
-      'hardwareBackPress',
-      backAction,
-    );
-
-    return () => backHandler.remove();
-  }, []);
-
   return (
     <WebView source={{uri: 'https://halalborder.com'}} style={{flex: 1}} />
   );
